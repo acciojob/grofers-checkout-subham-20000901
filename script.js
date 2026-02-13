b98b4411fea5528c2a9c192986a66aa8767a873f
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-13 22:00:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-
-  let prices = document.querySelectorAll(".prices");
+  let total = 0;
+  let prices = document.querySelectorAll(".price");
 	for(let i=0; i<prices.length; i++){
 		total += Number(prices[i].textContent);	
 	}
 	let newRow = document.createElement("tr");
 	let newCell = document.createElement("td");
-	newCell.colspan = 2;
+	newCell.colSpan = 2;
 	newCell.textContent = "Total price : " + total;
 	newRow.appendChild(newCell);
 	document.querySelector("table").appendChild(newRow);
